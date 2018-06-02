3c85a7540d1eff7f7485f04faf8886f53a3411c7
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-06-02 15:33:01 -0400

    create our first action which handle the inital data

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from './users'
+import { receivePolls } from './polls'
+import { setAuthedUser } from './authedUser'
+
+cost USER_ID = 'tylermcginnis'
+//thunk
+export function handleInitialData() {
+  return (dispatch) => {
+    getInitialData()
+    .then({users, polls} => {
+      dispatch(receiveUsers(users))
+      dispatch(receivePolls(polls))
+      dispatch(setAuthedUser(USER_ID))
+    })
+  }
+}
