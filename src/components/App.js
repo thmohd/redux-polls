c3a1fea1f4cb56a7f10f019050e42f419615ada1
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-06-03 13:58:00 -0400

    load our initial data - check console for details

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+
+  componentDidMount(){
+      this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +17,7 @@ class App extends Component {
   }
 }
 
-export default App
+
+const ConnectedApp = connect()(App)
+
+export default ConnectedApp
