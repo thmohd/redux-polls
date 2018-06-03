921f9bb7e9afacd7f0c6f41f2df9e0ac2edbdbb3
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2018-06-02 23:43:52 -0400

    install redux-thunk and apply our middleware with logger and thunk

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import App from './components/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import Reducers from './reducers'
+import Middleware from './middleware'
 
-const store = createStore(Reducers)
+const store = createStore(Reducers, Middleware)
+
+//console.log(store.getState())
 
 ReactDOM.render(
   <Provider store={store}>
